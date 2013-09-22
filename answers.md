fb6ae2a8e205e37c31193197512b80ddeaef7e46
Jake Kenneally <jdk2pq@virginia.edu>
2013-09-22 17:55:20 -0400

    Added history function.

---
+++ answers.md
@@ -0,0 +1,3 @@
+Title: Problem Set 2 Answers
+Authors: Jake Kenneally, Nick Skelsey
+
--- gash.rs
+++ gash.rs
@@ -2,19 +2,30 @@ use std::{io, run};
 
 fn main() {
     static CMD_PROMPT: &'static str = "gash > ";
+    let mut hist: ~[~str] = ~[]; 
     
     loop {
         print(CMD_PROMPT);
         let line = io::stdin().read_line();
+        hist.push(line.clone());
         debug!(fmt!("line: %?", line));
         let mut argv: ~[~str] = line.split_iter(' ').filter(|&x| x != "")
-                                 .transform(|x| x.to_owned()).collect();
+        .transform(|x| x.to_owned()).collect();
         debug!(fmt!("argv %?", argv));
         
         if argv.len() > 0 {
             let program = argv.remove(0);
             match program {
                 ~"exit"     => {return; }
+                ~"hist"     => {
+                    println(hist.len().to_str());
+                    let mut x = 0;
+                    while x < hist.len()
+                    {
+                        println(hist[x]);
+                        x++;
+                    }   
+                }
                 _           => {run::process_status(program, argv);}
             }
         }
