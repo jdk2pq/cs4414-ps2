ae7fa299f9e9afe19950f86d31444ec9d0e7ee7f
NSkelsey <NSkelsey@gmail.com>
2013-09-24 02:11:56 -0400

    framed out code for pipes and redirects, need to call processes with file_descriptors

--- gash.rs
+++ gash.rs
@@ -1,6 +1,52 @@
-use std::{io, run, os};
+use std::{io, run, os, vec};
+
+struct p_params  { // stores information to be passed into a process
+    program: ~str,
+    args: ~[~str]
+}
+
+fn parse_and_run(program: &str, args: &[~str]) { // will eventually need to return an int
+    // deal with args to figure out what needs to be done with fds
+    let mut first = p_params { program: program.to_owned(), args: ~[]};
+    let mut progs_to_run: ~[p_params] = ~[first];
+    let mut i = 0; 
+    for args.iter().advance |arg| {
+        let mut arg: ~str = arg.to_owned();
+        match arg {
+            ~"<"    => {},
+            ~">"    => {},
+            ~"|"    => {
+                let next_pstruct = p_params { program: ~"", args: ~[]};
+                progs_to_run.push(next_pstruct);
+                i += 1;
+                // deal with pipe setup
+            },
+            _       => {
+                // must copy out and in for some reason
+                // ASK SOMEONE DONT FORGET
+                // remove copy compiler throws:
+                // error: cannot move out of captured outer variable 
+                let mut cur_prog = copy progs_to_run[i];
+                if cur_prog.program != ~"" {
+                    println(fmt!("%?", arg));
+                    cur_prog.args.push(arg);
+                } else {
+                    cur_prog.program = arg;
+                }
+                progs_to_run[i] = cur_prog;
+            },
+        }
+    }
+    println(fmt!("%?", progs_to_run));
+
+    // spawn process
+    run::process_status(program, args);
+}
+
 
 fn main() {
+    // `static indicates that variable will have the static lifetime, meaing
+    // it gets to live for the whole life of the program
     static CMD_PROMPT: &'static str = "gash > ";
     let mut hist: ~[~str] = ~[]; 
     
@@ -40,23 +86,27 @@ fn main() {
                 }
                 _           => {
                     if argv.len() != 0 {
-                        let mut background = ~"";
+                        let mut background: ~str;
                         if argv.last() == &~"&" {
                             background = argv.pop();
                         } else {
                             background = ~"";
                         }
-                        let args = argv;
+                        // this line works because it changes the mutability of argv
+                        // so that the subproccess can confidently access values in argv
+                        let args: ~[~str] = argv; 
+                        // we can no longer modify argv, hence:
+                        // argv[0] = ~"foo"; //fails complitation
                         match background {
                             ~"&" => {
                                 do std::task::spawn_sched(std::task::SingleThreaded) { 
-                                    run::Process::new(program, args, run::ProcessOptions::new());
+                                    parse_and_run(program, args);
                                 }
                             }
-                            _ => {run::process_status(program, args);}
+                            _ => {parse_and_run(program, args);}
                         }
                     } else {
-                        run::process_status(program, argv);
+                        parse_and_run(program, argv)
                     }
 
                 }
